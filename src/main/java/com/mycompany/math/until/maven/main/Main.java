e9bd476e7053b8d9d68c355076e6ff9971d39554
Toannd832 <toan03182@gmail.com>
2023-01-13 21:40:24 +0700

    Toannd show hang Maven

---
+++ src/main/java/com/mycompany/math/until/maven/main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.math.until.maven.main;
+
+/**
+ *
+ * @author toan0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi all, "
+                + "This app is built by usng Maven");
+    }
+}
---
+++ src/test/java/com/vinsu/mathutil/core/mathUtilTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.vinsu.mathutil.core;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author toan0
+ */
+public class mathUtilTest {
+    
+    @Test 
+    public void testFractorialGivenRightArgumentreturnWell(){
+        assertEquals(20, 20);
+    }
+    
+}
